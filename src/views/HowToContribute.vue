c1576ecae77a068b152c4431a4a039294e149f13
lennartfynn <lennartfynnk@gmail.com>
2022-11-22 13:37:26 +0100

    edits about

--- src/views/HowToContribute.vue
+++ src/views/HowToContribute.vue
@@ -1,13 +1,57 @@
 <template>
-    <div>Page for how to contribute</div>
-</template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style scoped>
-
-</style>
+    <body>
+        <div id="div1" class="shadowbox"> 
+          <section>
+              <article>
+                <h1>How to contribute</h1>
+                  <p>We need you to contribute</p>
+              </article>
+          </section> 
+        </div> 
+  
+        <div id="div2" class="shadowbox">
+          <section>
+            <h2>About the authors</h2>
+              <article>
+                <p>This is an about Page</p>
+              </article>
+          </section> 
+        </div>
+    </body>
+  </template>
+  
+  
+  <style scoped>
+  body {
+    background-image: url('cartographyLogo.svg');
+  }
+  
+  #div1 {
+    width: 70%;
+    float: left;
+  }
+  
+  #div2 {
+    width: 30%;
+    float: right;
+  }
+  
+  .shadowbox {
+    border: 1px solid #333;
+    box-shadow: 8px 8px 5px #444;
+    padding: 8px 12px;
+    background-image: linear-gradient(180deg, #fff, #ddd 40%, #ccc);
+  }
+  
+  section {
+  
+  }
+  
+  article{
+  
+  }
+  
+  p {
+    color: #26b72b;
+  }
+  </style>
