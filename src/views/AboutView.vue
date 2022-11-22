c52bcf8c8937ff957db48ba3823dcad02f275892
lennartfynn <lennartfynnk@gmail.com>
2022-11-22 11:35:48 +0100

    edit about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <body>
+      <div class="shadowbox"> 
+        <h1> This an about page</h1>
+          <p> This section contains the content </p> 
+      </div> 
+  </body>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+
+<style scoped>
+body {
+  background-image: url('cartographyLogo.svg');
+}
+
+div {
+
+}
+.shadowbox {
+  width: 15em;
+  border: 1px solid #333;
+  box-shadow: 8px 8px 5px #444;
+  padding: 8px 12px;
+  background-image: linear-gradient(180deg, #fff, #ddd 40%, #ccc);
+}
+
+p {
+  color: #26b72b;
 }
 </style>
