db6ff96bca37451e1488de7ee101c735ae3995f2
Samuel Darkwah Manu <manusam14@yahoo.com>
2022-11-29 13:33:19 +0100

    structure text pages

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,73 +1,65 @@
 <template>
-  <body style="background-image: url('../assets/map.jpg');">
-      <div id="div1" class="shadowbox"> 
-        <section>
-            <article>
-              <h1>About this project</h1>
-                <h3>Motivation</h3>
-                  <p>Impaired people have difficulties participating many aspects of life as they struggle to reach places due to infrastructure that was not built to serve them<br>
-                    Information about accessibility is still scarce and unequally distributed around the globe<br>
-                    No central authority in charge of distributing data<br>
-                    Difficult to contribute and map data where it is needed<br></p>
-                <h3>Aim</h3>
-                  <p>Raise awareness to locations where there is little data on accessibility<br>
-                    Give incentive to contribute information to little covered areas<br></p>
-                <h3>Approach</h3>
-                  <p>Only centralized database with information<br>
-                    Information can be added (Key aim)<br>
-                    Independent of any another institution<br>
-                    Combination with other open-source projects<br>
-                    Add workflow graphic<br></p>
-                <h3>Map Design</h3>
-                  <p>Choice of Layers:<br>
-                    Choice of color scheme:<br></p>
-              </article>
-        </section> 
-      </div> 
-
-      <div id="div2" class="shadowbox">
-        <section>
-          <h2>About the authors</h2>
-            <article>
-              <p>This is an about page</p>
-              <img> XXX
-            </article>
-        </section> 
-      </div>
-  </body>
+  <div id="about-container">
+    <section id="side-nav">
+      <nav>
+        <a href="#background">Background</a>
+      </nav>
+    </section>
+    <section id="content">
+      <article id="background">
+        <h1>About this project</h1>
+        <h2>Motivation</h2>
+        <p>
+          Impaired people have difficulties participating many aspects of life
+          as they struggle to reach places due to infrastructure that was not
+          built to serve them<br />
+          Information about accessibility is still scarce and unequally
+          distributed around the globe<br />
+          No central authority in charge of distributing data<br />
+          Difficult to contribute and map data where it is needed<br />
+        </p>
+        <h2>Aim</h2>
+        <p>
+          Raise awareness to locations where there is little data on
+          accessibility<br />
+          Give incentive to contribute information to little covered areas<br />
+        </p>
+        <h2>Approach</h2>
+        <p>
+          Only centralized database with information<br />
+          Information can be added (Key aim)<br />
+          Independent of any another institution<br />
+          Combination with other open-source projects<br />
+          Add workflow graphic<br />
+        </p>
+        <h2>Map Design</h2>
+        <p>
+          Choice of Layers:<br />
+          Choice of color scheme:<br />
+        </p>
+      </article>
+    </section>
+  </div>
 </template>
 
-<style>
-
-body {
-  background-image: url('../assets/map.jpg');
-  background-size: 100%;
-  background-repeat: no-repeat;
+<style scoped>
+#about-container {
+  display: flex;
+  justify-content: space-between;
+  max-width: 80%;
+  margin: 3rem auto;
 }
 
-#div1 {
-  box-sizing: border-box;
-  width: 70%;
-  float: left;
-  text-align:left;
-  background-color: transparent;
+#side-nav {
+  width: 20%;
+  text-align: right;
+  position: fixed;
+  top: 8rem;
+  left: 1rem;
 }
 
-#div2 {
-  width: 30%;
-  float: right;
-  text-align: left;
-  background-color: transparent;
+#content {
+  width: 80%;
+  margin-left: 15rem;
 }
-
-.shadowbox {
-  border: 0.5px solid #333;
-  box-shadow: 2px 2px 1px rgb(233, 229, 229);
-  padding: 1px 1px;
-  /*background-image: linear-gradient(180deg, #fff, #ddd 40%, #ccc);*/
-}
-
-/*p {
-  color: #26b72b;
-}*/
 </style>
--- src/views/HowToContribute.vue
+++ src/views/HowToContribute.vue
@@ -1,57 +1,36 @@
 <template>
-    <body>
-        <div id="div1" class="shadowbox"> 
-          <section>
-              <article>
-                <h1>How to contribute</h1>
-                  <p></p>
-              </article>
-          </section> 
-        </div> 
-  
-        <div id="div2" class="shadowbox">
-          <section>
-            <h2>About the authors</h2>
-              <article>
-                <p>This is an about Page</p>
-              </article>
-          </section> 
-        </div>
-    </body>
-  </template>
-  
-  
-  <style scoped>
-  body {
-    background-image: url('cartographyLogo.svg');
-  }
-  
-  #div1 {
-    width: 70%;
-    float: left;
-  }
-  
-  #div2 {
-    width: 30%;
-    float: right;
-  }
-  
-  .shadowbox {
-    border: 1px solid #333;
-    box-shadow: 8px 8px 5px #444;
-    padding: 8px 12px;
-    background-image: linear-gradient(180deg, #fff, #ddd 40%, #ccc);
-  }
-  
-  section {
-  
-  }
-  
-  article{
-  
-  }
-  
-  p {
-    color: #26b72b;
-  }
-  </style>
+  <div id="about-container">
+    <section id="side-nav">
+      <nav>
+        <a href="#background">Contribute with mobile</a>
+      </nav>
+    </section>
+    <section id="content">
+      <article id="background">
+        <h1>Contribute with mobile</h1>
+      </article>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+#about-container {
+  display: flex;
+  justify-content: space-between;
+  max-width: 80%;
+  margin: 3rem auto;
+}
+
+#side-nav {
+  width: 20%;
+  text-align: right;
+  position: fixed;
+  top: 8rem;
+  left: 1rem;
+}
+
+#content {
+  width: 80%;
+  margin-left: 15rem;
+}
+</style>
