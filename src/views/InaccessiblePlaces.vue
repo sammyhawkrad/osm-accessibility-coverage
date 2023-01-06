f5f5588e314a3723d722dd8590cb45cc0bd4f4d1
Samuel Darkwah Manu <manusam14@yahoo.com>
2023-01-06 16:26:50 +0100

    fix umap url

--- src/views/InaccessiblePlaces.vue
+++ src/views/InaccessiblePlaces.vue
@@ -2,8 +2,8 @@
 import MapComponent from "../components/MapComponent.vue";
 import { ref, onMounted } from "vue";
 
-const src = ref("//http://umap.openstreetmap.fr/en/map/oac-inaccessible-places_849980");
-const src_dark = ref("http://umap.openstreetmap.fr/en/map/oac-inaccessible-places-dark_853492");
+const src = ref("//umap.openstreetmap.fr/en/map/oac-inaccessible-places_849980");
+const src_dark = ref("//umap.openstreetmap.fr/en/map/oac-inaccessible-places-dark_853492");
 
 const dark = ref(false);
 
