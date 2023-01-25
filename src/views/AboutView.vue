03db56cd6c0f6b7f0eca8fb5a65256e3d82b44c7
Samuel Darkwah Manu <manusam14@yahoo.com>
2023-01-25 04:55:29 +0100

    restructure info pages

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,155 +1,234 @@
 <template>
+  <div id="header-img">
+    <picture>
+      <source
+        srcset="/public/munich-light-large.png"
+        media="(min-width: 1400px)"
+      />
+      <img src="/public/munich-light-normal.png" alt="Map of Munich" />
+    </picture>
+  </div>
   <div id="about-container">
-    <section id="side-nav">
-      <nav>
-        <a href="#background"><li id="navli">Why Map?</li></a>
-        <a href="#approach"><li id="navli">Approach</li></a>
-        <a href="#workflow"><li id="navli">Workflow</li></a>
-        <a href="#themap"><li id="navli">How to use the map</li></a>
-        <a href="#databehindmap"><li id="navli">The data behind the map</li></a>
-      </nav>
-    </section>
     <section id="content">
-       
-        <article id="background">
+      <article id="background">
         <h2>Why did we map Accessibility?</h2>
-          <p>People with disabilities struggle to participate in many facets of life
-            and have trouble getting around because the infrastructure is not designed with them in mind.
-            Without information regarding a location's accessibility it is even harder to participate in social activities.
-            We discussed the mapping of accessibility data with the foundation Pfennigparade in Munich and one observation was
-            the uneven distribution of available data on accessibility.
-            In Munich's city centre there is a lot of data in comparison to rural or even suburban locations. 
-            We identified three main challenges in regard to this topic:</p>
-          <ol>
-            <li>Information about accessibility is still scarce and unequally distributed on every scale</li>
-            <li>No central authority in charge of distributing and maintaining data</li>
-            <li>Difficult to contribute and map data where it is needed</li>
-          </ol>
-          <p>We want to improve the situation and aim to:</p>
-          <ul>
-            <li>Raise awareness to regions, cities, districts and locations where there is little data on accessibility</li>
-            <li>Give incentive to contribute information to little covered areas</li>
-          </ul>
+        <p>
+          People with disabilities struggle to participate in many facets of
+          life and have trouble getting around because the infrastructure is not
+          designed with them in mind. Without information regarding a location's
+          accessibility it is even harder to participate in social activities.
+          We discussed the mapping of accessibility data with the foundation
+          Pfennigparade in Munich and one observation was the uneven
+          distribution of available data on accessibility. In Munich's city
+          centre there is a lot of data in comparison to rural or even suburban
+          locations. We identified three main challenges in regard to this
+          topic:
+        </p>
+        <ol>
+          <li>
+            Information about accessibility is still scarce and unequally
+            distributed on every scale
+          </li>
+          <li>
+            No central authority in charge of distributing and maintaining data
+          </li>
+          <li>Difficult to contribute and map data where it is needed</li>
+        </ol>
+        <p>We want to improve the situation and aim to:</p>
+        <ul>
+          <li>
+            Raise awareness to regions, cities, districts and locations where
+            there is little data on accessibility
+          </li>
+          <li>
+            Give incentive to contribute information to little covered areas
+          </li>
+        </ul>
       </article>
-      
+
       <article id="approach">
         <h2>Approach</h2>
-          <p> 
-            This project is based on OpenSource resources.
-			       We believe that it suits this project the most because it has multiple advantages:</p>
-			     <ul>
-            <li>Only centralized database with information on a global scale</li>
-			      <li>Independent of any another institution</li>
-			      <li>Information can be added (Key aim)</li>
-			      <li>Combination with other open-source projects</li>
-          </ul>
+        <p>
+          This project is based on OpenSource resources. We believe that it
+          suits this project the most because it has multiple advantages:
+        </p>
+        <ul>
+          <li>Only centralized database with information on a global scale</li>
+          <li>Independent of any another institution</li>
+          <li>Information can be added (Key aim)</li>
+          <li>Combination with other open-source projects</li>
+        </ul>
       </article>
 
       <article id="workflow">
         <h2>Workflow</h2>
-          <p>
-            <img src= "/src/assets/gomap/workflow.png" alt="Image that shows the workflow" width="600" height="auto">
-          </p>
+        <p>
+          <img
+            id="workflow"
+            src="/src/assets/gomap/workflow.png"
+            alt="Image that shows the workflow"
+            width="600"
+            height="auto"
+          />
+        </p>
       </article>
-      
+
       <article id="themap">
         <h2>How to use the Map</h2>
-        <p>This text shall provide a short explanation of how to use the map in case you are new to Umap.</p><br />
-            <h3>The Layout:</h3>
-            <p>
-            The map is by default a standard web map that is centered to Northern France.
-            You can either use it in light or dark mode.
-            When you zoom into an area of your choice, one layer will be displayed by default.
-            The layer will be displayed from a certain zoom level on.
-            The defined zoom level is a compromise between overview of the data and loading capabilities with an average internet connection.
-            The color scheme you see is the default setting of Umap:
-            A place with little available data is displayed as blue while changing to green,
-            yellow and eventually red with increasing number of data points.
-            The color is not connected to quality of the data,
-            meaning that red only means there is a lot of data and not that a place is more/less accessible or inaccessible.
-            Places with no information stay white/dark.
-            </p>
-          <p>
-            Most of the navigation bar functions are straight forward.
-            If you want to activate or deactivate a layer,
-            you open the See data Layers pan and click on the eye symbol of the layer you are interested in.
-            Next to it you will find a zoom icon, which does not work in this configuration of the map,
-            as it is not possible to zoom on all the information a layer provides. </p><br />
-            <h3>Home:</h3>
-            <p>The map displayed on Home, shows all the data points we collected regarding accessibility and inaccessibility.
-            It is categorized by objects, e.g. buildings, crossing, etc.
-            These categories are also the Layers of the map, which you can activate and deactivate.</p><br />
-            <h3>Accessible/Inaccessible Places:</h3>
-            <p>This map displays all the data that we connected to either accessible OR inaccessible places.
-            The information is categorized by impairments, e.g., visual, deaf or mobility impairment.</p>
+        <p>
+          This text shall provide a short explanation of how to use the map in
+          case you are new to Umap.
+        </p>
+        <br />
+        <h3>The Layout:</h3>
+        <p>
+          The map is by default a standard web map that is centered to Northern
+          France. You can either use it in light or dark mode. When you zoom
+          into an area of your choice, one layer will be displayed by default.
+          The layer will be displayed from a certain zoom level on. The defined
+          zoom level is a compromise between overview of the data and loading
+          capabilities with an average internet connection. The color scheme you
+          see is the default setting of Umap: A place with little available data
+          is displayed as blue while changing to green, yellow and eventually
+          red with increasing number of data points. The color is not connected
+          to quality of the data, meaning that red only means there is a lot of
+          data and not that a place is more/less accessible or inaccessible.
+          Places with no information stay white/dark.
+        </p>
+        <p>
+          Most of the navigation bar functions are straight forward. If you want
+          to activate or deactivate a layer, you open the See data Layers pan
+          and click on the eye symbol of the layer you are interested in. Next
+          to it you will find a zoom icon, which does not work in this
+          configuration of the map, as it is not possible to zoom on all the
+          information a layer provides.
+        </p>
+        <br />
+        <h3>Home:</h3>
+        <p>
+          The map displayed on Home, shows all the data points we collected
+          regarding accessibility and inaccessibility. It is categorized by
+          objects, e.g. buildings, crossing, etc. These categories are also the
+          Layers of the map, which you can activate and deactivate.
+        </p>
+        <br />
+        <h3>Accessible/Inaccessible Places:</h3>
+        <p>
+          This map displays all the data that we connected to either accessible
+          OR inaccessible places. The information is categorized by impairments,
+          e.g., visual, deaf or mobility impairment.
+        </p>
       </article>
-      
+
       <article id="databehindmap">
         <h2>The data behind the map</h2>
-          <p>
-            The tags of our data collection is based on the following criteria to assure quality and relevance of our data:</p>
-            <ul>
-              <li>The tag is named in the OSM Wiki.</li>
-              <li>The tag is used in a related project.</li>
-              <li>The tag is frequently used: 0,05 % of all related tags according to TagInfo</li>
-              <li>The tag can be identified as either accessible or inaccessible.</li>
-            </ul> 
-          <p>
-            We decided to work with 3 categories of impairment because all the tags we found were related to these.
-            Other disabilities could not be connected to the identified tags.
-            You can find the breakdown of tags and layers <a href='src\assets\taglist310123.pdf' download="taglist">here</a>.</p>
-            <br />
-            Follow this link to the <a href="https://github.com/sammyhawkrad/osm-accessibility-coverage">GitHub repository.</a>
+        <p>
+          The tags of our data collection is based on the following criteria to
+          assure quality and relevance of our data:
+        </p>
+        <ul>
+          <li>The tag is named in the OSM Wiki.</li>
+          <li>The tag is used in a related project.</li>
+          <li>
+            The tag is frequently used: 0,05 % of all related tags according to
+            TagInfo
+          </li>
+          <li>
+            The tag can be identified as either accessible or inaccessible.
+          </li>
+        </ul>
+        <p>
+          We decided to work with 3 categories of impairment because all the
+          tags we found were related to these. Other disabilities could not be
+          connected to the identified tags. You can find the breakdown of tags
+          and layers
+          <a href="src\assets\taglist310123.pdf" download="taglist">here</a>.
+        </p>
+        <br />
+        Follow this link to the
+        <a href="https://github.com/sammyhawkrad/osm-accessibility-coverage"
+          >GitHub repository.</a
+        >
       </article>
-    
+    </section>
+    <section id="sidebar">
+      <aside>
+        <h2>About the developers</h2>
+        <p>
+          This website was developed by Lennart Kerl and Samuel Darkwah Manu as
+          their Mapping Project in the first semester of the International
+          Master of Science in Cartography (12th intake).
+        </p>
+        <div class="profile">
+          <figure>
+            <img src="" alt="Picture of Lennart Kerl">
+            <figcaption>Lennart Kerl</figcaption>
+          </figure>
+        </div>
+        <div class="profile">
+          <figure>
+            <img src="" alt="Picture of Samuel Darkwah Manu">
+            <figcaption>Samuel Darkwah Manu</figcaption>
+          </figure>
+        </div>
+      </aside>
     </section>
   </div>
 </template>
 
 <style scoped>
+#header-img {
+  height: 300px;
+  width: 100%;
+  contain: strict;
+}
+
+@media (prefers-color-scheme: dark) {
+  #header-img img {
+    filter: grayscale(100%);
+  }
+}
+
 #about-container {
   display: flex;
   justify-content: space-between;
-  max-width: 80%;
-  margin: 3rem auto;
+  width: 90%;
+  padding-top: 3rem;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
 }
 
-#side-nav {
-  width: 20%;
-  text-align: right;
-  position: fixed;
-  top: 8rem;
-  left: 1rem;
+@media screen and (min-width: 1800px) {
+  #about-container {
+    width: 70%;
+  }
 }
 
-#navli {
+#sidebar {
   list-style-type: none;
-  margin: 1rem;
-  padding-bottom: 5px;
-  border-bottom: 1px solid lightgray;
+  width: 30%;
+  padding: 0 1rem;
 }
 
 #content {
-  width: 80%;
-  margin-left: 15rem;
+  width: 70%;
+  margin-right: 1rem;
+  padding: 0 5rem;
 }
 
-article{
+article {
   padding-bottom: 10px;
   margin-bottom: 5px;
   border-bottom: 1px solid lightgray;
 }
 
-img{
-  padding: 5px;
-  margin-left: 10rem;
-}
 
 ol {
   padding-top: 5px;
   margin-bottom: 5px;
   text-align: justify;
-  list-style-type:decimal;
+  list-style-type: decimal;
 }
 
 ul {
@@ -158,7 +237,7 @@ ul {
   list-style-type: circle;
 }
 
-p{
+/* p {
   text-align: justify;
-}
+} */
 </style>
--- src/views/HowToContribute.vue
+++ src/views/HowToContribute.vue
@@ -1,13 +1,14 @@
 <template>
+  <div id="header-img">
+    <picture>
+      <source
+        srcset="/public/munich-light-large.png"
+        media="(min-width: 1400px)"
+      />
+      <img src="/public/munich-light-normal.png" alt="Map of Munich" />
+    </picture>
+  </div>
   <div id="about-container">
-    <section id="side-nav">
-      <nav>
-        <a href="#OSM"><li id="navli">OpenStreetMap</li></a>
-        <a href="#StreetComplete"><li id="navli">StreetComplete</li></a>
-        <a href="#GoMap"><li id="navli">GoMap!!</li></a>
-      </nav>
-    </section>
-    
     <section id="content">
       <article id="OSM">
         <h2>OpenStreetMap</h2>
@@ -24,94 +25,139 @@
           <a href="https://www.openstreetmap.org/user/new">OSM Wiki</a><br />
           Or the Step to step OSM guide by
           <a href="https://learnosm.org/en">Learn OSM</a>!
-        </p><br />
+        </p>
+        <br />
       </article>
-    
+
       <article>
-      <h2>StreetComplete Guide</h2>
-      <p>Follow this guide to start mapping with StreetComplete on your Android phone.</p>
-      <div class="galleryimg" id="StreetComplete">
-            <img src= "/src/assets/gomap/guideimageSC123.png" alt="Images to show how to use Streetcomplete" width="600" height="auto">
-        <div class="desc">
-          Step 1: The GoMap!! interface is a map with a cross in its center. 
-          You can add information to the places where the cross is by pressing the + button. <br />
-          Step 2: You can either add attributes or add a hint. To add a tag, press attribute. <br />
-          Step 3: Add a key and its value, press save. In this example, the surface gravel was tagged. <br />
-        </div>
-      </div>
-     
-      <div class="galleryimg2">
-            <img src= "/src/assets/gomap/guideimageSC456X.png" alt="Images to show how to use Streetcomplete" width="600" height="auto">
+        <h2>StreetComplete Guide</h2>
+        <p>
+          Follow this guide to start mapping with StreetComplete on your Android
+          phone.
+        </p>
+        <div class="galleryimg" id="StreetComplete">
+          <img
+            src="/src/assets/gomap/guideimageSC123.png"
+            alt="Images to show how to use Streetcomplete"
+            width="600"
+            height="auto"
+          />
           <div class="desc">
-          Step 4: The GoMap!! interface is a map with a cross in its center. 
-          You can add information to the places where the cross is by pressing the + button. <br />
-          Step 5: You can either add attributes or add a hint. To add a tag, press attribute. <br />
-          Step 6: Add a key and its value, press save. In this example, the surface gravel was tagged. <br />
+            Step 1: The GoMap!! interface is a map with a cross in its center.
+            You can add information to the places where the cross is by pressing
+            the + button. <br />
+            Step 2: You can either add attributes or add a hint. To add a tag,
+            press attribute. <br />
+            Step 3: Add a key and its value, press save. In this example, the
+            surface gravel was tagged. <br />
+          </div>
         </div>
-      </div>
-    </article>
-   
-    <article>
-      <h2>GoMap!! Guide</h2>
-      <p>Follow this guide to start mapping with GoMap!! on your Iphone.</p>
-      <div class="galleryimg" id="GoMap">
-          <img src= "/src/assets/gomap/guideimageStep123.png" alt="Images to show how to use GoMap!!" width="600" height="auto">
-        <div class="desc">
-          Step 1: The GoMap!! interface is a map with a cross in its center. 
-          You can add information to the places where the cross is by pressing the + button. <br />
-          Step 2: You can either add attributes or add a hint. To add a tag, press attribute. <br />
-          Step 3: Add a key and its value, press save. In this example, the surface gravel was tagged. <br />
+
+        <div class="galleryimg2">
+          <img
+            src="/src/assets/gomap/guideimageSC456X.png"
+            alt="Images to show how to use Streetcomplete"
+            width="600"
+            height="auto"
+          />
+          <div class="desc">
+            Step 4: The GoMap!! interface is a map with a cross in its center.
+            You can add information to the places where the cross is by pressing
+            the + button. <br />
+            Step 5: You can either add attributes or add a hint. To add a tag,
+            press attribute. <br />
+            Step 6: Add a key and its value, press save. In this example, the
+            surface gravel was tagged. <br />
+          </div>
         </div>
-      </div>
-      
-      <div class="galleryimg2">
-          <img src= "/src/assets/gomap/guideimageStep45.png" alt="Images to show how to use GoMap!!" width="600" height="auto">
-        <div class="desc">
-        Step 4: To upload the tag press the cloud symbol in the downright corner. <br />
-        Step 5: Now add your comment and source. Press upload. <br />
+      </article>
+
+      <article>
+        <h2>GoMap!! Guide</h2>
+        <p>Follow this guide to start mapping with GoMap!! on your Iphone.</p>
+        <div class="galleryimg" id="GoMap">
+          <img
+            src="/src/assets/gomap/guideimageStep123.png"
+            alt="Images to show how to use GoMap!!"
+            width="600"
+            height="auto"
+          />
+          <div class="desc">
+            Step 1: The GoMap!! interface is a map with a cross in its center.
+            You can add information to the places where the cross is by pressing
+            the + button. <br />
+            Step 2: You can either add attributes or add a hint. To add a tag,
+            press attribute. <br />
+            Step 3: Add a key and its value, press save. In this example, the
+            surface gravel was tagged. <br />
+          </div>
         </div>
-      
-      </div>
-    </article>
-  </section>
+
+        <div class="galleryimg2">
+          <img
+            src="/src/assets/gomap/guideimageStep45.png"
+            alt="Images to show how to use GoMap!!"
+            width="600"
+            height="auto"
+          />
+          <div class="desc">
+            Step 4: To upload the tag press the cloud symbol in the downright
+            corner. <br />
+            Step 5: Now add your comment and source. Press upload. <br />
+          </div>
+        </div>
+      </article>
+    </section>
+
+    <section id="sidebar">
+      <aside>
+        <h2>Useful Resources</h2>
+      </aside>
+    </section>
   </div>
 </template>
 
 <style scoped>
-#about-container {
-  display: flex;
-  justify-content: space-between;
-  max-width: 80%;
-  margin: 3rem auto;
-  position: relative;
-}
-
-#side-nav {
-  list-style-type: none;
-  width: 20%;
-  text-align: right;
-  position: fixed;
-  top: 8rem;
-  left: 1rem;
-}
-
-#navli {
-  list-style-type: none;
-  margin: 1rem;
-  padding: 5px;
-  border-bottom: 1px solid lightgray;
+#header-img {
+  height: 300px;
+  width: 100%;
+  contain: strict;
 }
 
 @media (prefers-color-scheme: dark) {
-  #navli {
-    border-bottom: 1px solid gray;
+  #header-img img {
+    filter: grayscale(100%);
   }
 }
 
-#content {
-  width: 80%;
-  margin-left: 15rem;
+#about-container {
+  display: flex;
+  justify-content: space-between;
+  width: 90%;
+  padding-top: 3rem;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
 }
+
+@media screen and (min-width: 1800px) {
+  #about-container {
+    width: 70%;
+  }
+}
+
+#sidebar {
+  list-style-type: none;
+  width: 30%;
+  padding: 0 1rem;
+}
+
+#content {
+  width: 70%;
+  margin-right: 1rem;
+  padding: 0 5rem;
+}
+
 article {
   border-bottom: 1px solid lightgray;
 }
@@ -135,5 +181,4 @@ div.desc {
   position: relative;
   width: 100%;
 }
-
 </style>
